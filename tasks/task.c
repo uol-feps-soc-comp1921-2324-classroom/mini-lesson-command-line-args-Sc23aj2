cc76d4d86e4383ac0f977df19a7f85715fce25c3
Sc23aj2 <146622139+Sc23aj2@users.noreply.github.com>
2024-02-01 07:20:49 +0000

    mini lesson 1

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int num1, num2;
+/**
+ *Prints out sum of two numbers
+ *argv[1] = num1
+ *argv[2] = num2
+ */
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+int main(int argc, char *argv[]) {
+
+    if (argc != 3){
+        printf("Usage: %s <num1> <num2>\n", argv[0]);
+        return 0;
+    }
+
+    // Converting the strings into numbers
+     int num1 = atoi(argv[1]);
+     int num2 = atoi(argv[2]);
 
     // Calculate and print the sum
     int sum = num1 + num2;
